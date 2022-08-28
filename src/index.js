b8b6a294cf57181c39536ec8d939952d17ab55e7
yuchan1120 <nogginpanic@gmail.com>
2022-08-29 02:44:13 +0900

    JSXに式(値)を埋め込み

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 const MoneyBook = () => {
+  const books = [
+    {date: "1/1", item: "お年玉", amount: 10000},
+    {date: "1/3", item: "ケーキ", amount: -500},
+    {date: "2/1", item: "小遣い", amount: 3000},
+    {date: "2/5", item: "マンガ", amount: -600}]
   return (
     <div>
       <h1>小遣い帳</h1>
@@ -11,10 +16,10 @@ const MoneyBook = () => {
           <tr><th>日付</th><th>項目</th><th>入金</th><th>出金</th></tr>
         </thead>
         <tbody>
-          <tr><td>1/1</td><td>お年玉</td><td>10000</td><td></td></tr>
-          <tr><td>1/3</td><td>ケーキ</td><td></td><td>500</td></tr>
-          <tr><td>2/1</td><td>小遣い</td><td>3000</td><td></td></tr>
-          <tr><td>2/5</td><td>マンガ</td><td></td><td>600</td></tr>
+          <tr><td>{books[0].date}</td><td>{books[0].item}</td><td>{books[0].amount}</td><td></td></tr>
+          <tr><td>{books[1].date}</td><td>{books[1].item}</td><td></td><td>{-books[1].amount}</td></tr>
+          <tr><td>{books[2].date}</td><td>{books[2].item}</td><td>{books[2].amount}</td><td></td></tr>
+          <tr><td>{books[3].date}</td><td>{books[3].item}</td><td></td><td>{-books[3].amount}</td></tr>
         </tbody>
       </table>
     </div>
