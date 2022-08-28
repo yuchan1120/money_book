96211f2c6e75c265207748368e81745f35db9988
yuchan1120 <nogginpanic@gmail.com>
2022-08-29 03:11:56 +0900

    条件演算子を使いMoneyBookItemコンポーネントを修正

--- src/index.js
+++ src/index.js
@@ -29,25 +29,14 @@ const MoneyBook = () => {
 
 const MoneyBookItem = (props) => {
   const {date, item, amount} = props.book
-  if (amount > 0) {
-    return (
-      <tr>
-        <td>{date}</td>
-        <td>{item}</td>
-        <td>{amount}</td>
-        <td></td>
-      </tr>
-    )
-  } else {
-    return (
-      <tr>
-        <td>{date}</td>
-        <td>{item}</td>
-        <td></td>
-        <td>{-amount}</td>
-      </tr>
-    )
-  }
+  return (
+    <tr>
+      <td>{date}</td>
+      <td>{item}</td>
+      <td>{amount >= 0 ? amount : null}</td>
+      <td>{amount < 0  ? -amount : null}</td>
+    </tr>
+  )
 }
 MoneyBookItem.propTypes = {
   book: PropTypes.object.isRequired
