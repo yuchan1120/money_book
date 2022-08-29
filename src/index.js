1bada330716cea945c9723186f1018617c6bfe9b
yuchan1120 <nogginpanic@gmail.com>
2022-08-29 20:35:15 +0900

    子要素を扱うコンポーネントを追加

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const MoneyBook = () => {
     {date: "2/5", item: "マンガ", amount: -600}]
   return (
     <div>
-      <h1>小遣い帳</h1>
+      <Title>小遣い帳</Title>
       <table className="book">
         <thead>
           <tr><th>日付</th><th>項目</th><th>入金</th><th>出金</th></tr>
@@ -43,6 +43,13 @@ const MoneyBookItem = (props) => {
 }
 MoneyBookItem.propTypes = {
   book: PropTypes.object.isRequired
+}
+
+const Title = (props) => {
+  return (<h1>{props.children}</h1>)
+}
+Title.propTypes = {
+  children: PropTypes.string
 }
 
 ReactDOM.render(
