6e59fa40cfaac940fc3266495682a55d918fdcb0
yuchan1120 <nogginpanic@gmail.com>
2022-08-30 21:58:10 +0900

    データを追加できる機能を追加

--- src/index.js
+++ src/index.js
@@ -1,33 +1,99 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import './index.css'
 
-const MoneyBook = () => {
-  const books = [
+class MoneyBook extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {books: []}
+  }
+  componentDidMount() {
+    this.setState({books: [
     {date: "1/1", item: "お年玉", amount: 10000},
     {date: "1/3", item: "ケーキ", amount: -500},
     {date: "2/1", item: "小遣い", amount: 3000},
-    {date: "2/5", item: "マンガ", amount: -600}]
+    {date: "2/5", item: "マンガ", amount: -600}
+    ]})
+  }
+  addBook(date, item, amount) {
+    const book = {date: date, item: item, amount: amount}
+    this.setState({books: this.state.books.concat(book)})
+  }
+  render() {
+    return (
+      <div>
+        <Title>小遣い帳</Title>
+        <MoneyBookList books={this.state.books} />
+        <MoneyEntry add={(date, item, amount) => this.addBook(date, item, amount)} />
+      </div>
+    )
+  }
+}
+
+class MoneyEntry extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: '', item: '', amount: '', payingIn: true}
+  }
+  onChangeDate(event) {
+    this.setState({date: event.target.value})
+  }
+  onChangeItem(event) {
+    this.setState({item: event.target.value})
+  }
+  onChangeAmount(event) {
+    this.setState({amount: event.target.value})
+  }
+  onChanePayingIn(event) {
+    this.setState({payingIn: event.target.value == "on"})
+  }
+  onClickSubmit() {
+    this.props.add(this.state.date, this.state.item, this.state.amount * (this.state.payingIn ? 1 : -1))
+    this.setState({date: '', item: '', amount: '', payingIn: false})
+  }
+  render() {
+    return (
+      <div className="entry">
+        <fieldset>
+          <legend>記帳</legend>
+          <div>
+            <input type="radio" value="on" checked={this.state.payingIn} onChange={(event) => this.onChanePayingIn(event)} /> 入金
+            <input type="radio" value="off" checked={!this.state.payingIn} onChange={(event) => this.onChanePayingIn(event)} /> 出金
+          </div>
+          <div>日付: <input type="text" value={this.state.date} onChange={(event) => this.onChangeDate(event)} placeholder="3/15" /> </div>
+          <div>項目: <input type="text" value={this.state.item} onChange={(event) => this.onChangeItem(event)} placeholder="おこづかい" /> </div>
+          <div>金額: <input type="text" value={this.state.amount} onChange={(event) => this.onChangeAmount(event)} placeholder="1000" /> </div>
+          <div> <input type="submit" value="追加" onClick={() => this.onClickSubmit()} /> </div>
+        </fieldset>
+      </div>
+    )
+  }
+}
+MoneyEntry.propTypes = {
+  add: PropTypes.func.isRequired
+}
+
+const MoneyBookList = (props) => {
   return (
     <div>
-      <Title>小遣い帳</Title>
       <table className="book">
-        <thead>
+        <thead  data-type="ok">
           <tr><th>日付</th><th>項目</th><th>入金</th><th>出金</th></tr>
         </thead>
         <tbody>
-          {books.map((book) =>
+          {props.books.map((book) =>
             <MoneyBookItem book={book} key={book.date + book.item} /> )}
-            {/*
-              {books.map((book) =>
+          {/*{books.map((book) =>
               <MoneyBookItem book={book} key={book.id} /> )}
-              と書くこともできる
-            */}
+              と書くこともできる*/}
         </tbody>
       </table>
     </div>
   )
+}
+MoneyBookList.propTypes = {
+  books: PropTypes.array.isRequired
 }
 
 const MoneyBookItem = (props) => {
